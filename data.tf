e714290c7978b9e685899c2a6ef04501497f158a
Watchrapong Agsonchu <watchrapongagsonchu@ip-10-150-0-123.ap-southeast-1.compute.internal>
2020-03-13 15:33:05 +0700

    Add module_depends_on to data_resource, to force them wait resource succcesfully created before getting data

--- data.tf
+++ data.tf
@@ -5,6 +5,9 @@ data "aws_vpc" "vpc" {
   tags = {
     "Name" = var.vpc
   }
+  depends_on = [
+    null_resource.module_depends_on
+  ]
 }
 
 data "aws_region" "current" {}
@@ -15,7 +18,10 @@ data "aws_subnet_ids" "public" {
   filter {
     name   = "tag:${var.subnet_tag}"
     values = [var.public_subnets]
-  }
+  },
+  depends_on = [
+    null_resource.module_depends_on
+  ]
 }
 
 data "aws_subnet_ids" "private" {
@@ -24,7 +30,10 @@ data "aws_subnet_ids" "private" {
   filter {
     name   = "tag:${var.subnet_tag}"
     values = [var.private_subnets]
-  }
+  },
+  depends_on = [
+    null_resource.module_depends_on
+  ]
 }
 
 data "aws_security_group" "default" {
@@ -32,25 +41,43 @@ data "aws_security_group" "default" {
 
   tags = {
     "Name" = var.default_security_group
-  }
+  },
+  depends_on = [
+    null_resource.module_depends_on
+  ]
 }
 
 data "aws_acm_certificate" "external-cert" {
-  domain = var.ssl_cert_external
+  domain = var.ssl_cert_externa
+  depends_on = [
+    null_resource.module_depends_on
+  ]
 }
 
 data "aws_acm_certificate" "internal-cert" {
   domain = var.ssl_cert_internal
+  depends_on = [
+    null_resource.module_depends_on
+  ]
 }
 
 data "aws_acm_certificate" "admin-cert" {
   domain = var.ssl_cert_admin
+  depends_on = [
+    null_resource.module_depends_on
+  ]
 }
 
 data "aws_acm_certificate" "manager-cert" {
   domain = var.ssl_cert_manager
+  depends_on = [
+    null_resource.module_depends_on
+  ]
 }
 
 data "aws_acm_certificate" "portal-cert" {
   domain = var.ssl_cert_portal
+  depends_on = [
+    null_resource.module_depends_on
+  ]
 }
