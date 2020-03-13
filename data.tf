ad341b6a841cc66efcad40b6de4e4a740733cdd9
Watchrapong Agsonchu <watchrapongagsonchu@ip-10-150-0-123.ap-southeast-1.compute.internal>
2020-03-13 15:36:45 +0700

    Fix unexpected additional colon

--- data.tf
+++ data.tf
@@ -18,7 +18,7 @@ data "aws_subnet_ids" "public" {
   filter {
     name   = "tag:${var.subnet_tag}"
     values = [var.public_subnets]
-  },
+  }
   depends_on = [
     null_resource.module_depends_on
   ]
@@ -30,7 +30,7 @@ data "aws_subnet_ids" "private" {
   filter {
     name   = "tag:${var.subnet_tag}"
     values = [var.private_subnets]
-  },
+  }
   depends_on = [
     null_resource.module_depends_on
   ]
@@ -41,7 +41,7 @@ data "aws_security_group" "default" {
 
   tags = {
     "Name" = var.default_security_group
-  },
+  }
   depends_on = [
     null_resource.module_depends_on
   ]
